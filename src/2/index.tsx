95b0b097dcc07272a2aa4714888593b374a26ecb
Selim <selimabdelmonsef@gmail.com>
2023-09-02 23:35:52 +0300

    feat(index.tsx/2): added TableComponent in index.tsx with mock data

--- src/2/index.tsx
+++ src/2/index.tsx
@@ -1,17 +1,31 @@
 import { FunctionComponent } from "react";
+import { TableComponent } from "../components/core/TableComponent/TableComponent";
+import { RowProps } from "../components/core/TableComponent/TableComponent.types";
 
-// Components
-import Input from "./components/Input";
-import List from "./components/List";
-
+/**
+ * Used TableComponent customized for retrieving list of items with search input
+ */
 const Task2: FunctionComponent = () => {
-  return (
-    <div>
-      <Input />
-      <br />
-      <List />
-    </div>
-  );
+  let columns = [
+    {
+      cell: "First Name",
+    },
+    {
+      cell: "Last Name",
+    },
+    {
+      cell: "Position",
+    },
+    {
+      cell: "Age",
+    },
+  ];
+  let rows: RowProps[] = [
+    { cells: ["Selim", "Abdelmonsef", "Frontend Developer", "31"] },
+    { cells: ["Hoda", "Elmehi", "Project Manager", "29"] },
+    { cells: ["Youssef", "Yasser", "Backend Developer", "32"] },
+  ];
+  return <TableComponent rows={rows} columns={columns} />;
 };
 
 export default Task2;
